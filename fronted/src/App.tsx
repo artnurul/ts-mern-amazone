4298dd114e1c2cdc1d2d08b01840e831716593a2
artnurul <artnurul2@gmail.com>
2023-06-02 10:18:59 +0700

    first commit

---
+++ fronted/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>Semangat jet</header>
+      <main></main>
+      <footer>Iqra' bacalah atas nama tuhanmu</footer>
+    </div>
+  )
+}
+
+export default App
